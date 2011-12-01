1d9086d5d54357ede8e0c891a013a4531ac78206
Mike Fulcher <mike@plan9design.co.uk>
2011-12-01 16:58:04 +0000

    Move into /public for testing, and enhance

--- public/jquery.mobile.slider.js
+++ public/jquery.mobile.slider.js
@@ -30,6 +30,36 @@
 })(jQuery);
 
 /**
+ * simulate tap event (iOS click events are delayed)
+ */
+(function($) {
+  $.event.special.tap = {
+    setup: function() {
+      var thisObject = this,
+          $this = $(thisObject),
+          timer, down = false;
+        
+      $this.bind('mousedown touchstart', function(e) {
+        if (e.which && e.which !== 1) { return false; }
+        e.preventDefault();
+        
+        down = true;
+        timer = setTimeout(function() { down = false; }, 250);
+        
+        $this.bind('mouseup touchend', function(e) {
+          if (down) {
+            $this.trigger('tap', e);
+            clearTimeout(timer);
+            down = false;
+          }
+          $this.unbind('mouseup touchend');
+        });
+      });
+    }
+  };
+})(jQuery);
+
+/**
  * mobile slider by Mike Fulcher
  */
 (function($){
@@ -53,7 +83,8 @@
                   '</div>';
                   
   var doc = $(document),
-      mousedown = false;
+      mousedown = false,
+      tapTimer;
       
   doc.bind('mousedown', function(e) {
     if (e.which <= 1 && !e.metaKey && !e.shiftKey && !e.altKey && !e.ctrlKey) {
@@ -65,12 +96,14 @@
   
   // when releasing the handle, "snap" the slider
   doc.bind('mouseup touchend touchcancel', function() {
-    $('.ui-slideToggle').each(function(i, slider) {
+    $('.ui-slideToggle[data-dragging=true]').each(function(i, slider) {
       slider = $(slider);
-      if (slider.find('.ui-slideToggle-handle').offset().left + 15 > slider.data('center').left) {
-        slider.data('controls').on();
-      } else {
-        slider.data('controls').off();
+      if (!slider.data('animating')) {
+        if (slider.find('.ui-slideToggle-handle').offset().left + 15 > slider.data('center').left) {
+          slider.data('controls').on();
+        } else {
+          slider.data('controls').off();
+        }
       }
     });
   });
@@ -118,7 +151,7 @@
       slider.find('a').width(labelMaxWidth);
       
       // cache the "off" and "on" positions
-      var masterOff = '-' + (labelMaxWidth + 20) + 'px',
+      var masterOff = '-' + (labelMaxWidth + 19) + 'px',
           masterOn  = '0px';
       
       // default to the "off" position
@@ -134,11 +167,18 @@
       select.data('slider', slider);
       slider.data('select', select);
       
+      // helpers to determine if the slider is currently in motion or being dragged
+      slider.data('animating', false).attr('data-dragging', 'false');
+      
       // cache the offset, dimensions and center point of the slider widget
       slider
         .data('offset', slider.offset())
         .data('dimensions', { width: slider.width(), height: slider.height() })
         .data('center', { left: slider.data('offset').left + (slider.data('dimensions').width / 2), top: slider.data('offset').top + (slider.data('dimensions').height / 2) });
+      
+      // cache the offset of the "master" and "handle" elements
+      master.data('offset', master.offset());
+      handle.data('offset', handle.offset());
       
       // add controls to the slider widget
       slider.data('controls', {
@@ -152,20 +192,50 @@
       });
       
       // tap to toggle
-      slider.bind('click:left tap', function(e) {
+      slider.bind('tap', function(e) {
         e.preventDefault();
         
-        if (!$(e.target).is('span')) {
-          slider.data('controls')[slider.hasClass('on') ? 'off' : 'on']();
+        if (true) {
+          if (!$(e.target).is('span')) {
+            slider.data('controls')[slider.hasClass('on') ? 'off' : 'on']();
+          }
         }
+        
+        mousedown = false;
+      });
+      
+      // "grab" the slider at a certain point when dragging starts
+      slider.bind('mousedown touchstart', function(e) {
+        e.preventDefault();
+        
+        // normalize the pageX, pageY coordinates
+        var pageX, pageY;
+        if (e.type == "touchstart") {
+          pageX = e.originalEvent.targetTouches[0].pageX;
+          pageY = e.originalEvent.targetTouches[0].pageY;
+        } else {
+          pageX = e.pageX;
+          pageY = e.pageY;
+        }
+        
+        // calculate the offset based on which part of the slider was grabbed
+        var masterLeft = parseInt(master.css('left')), // -37..0
+            masterOffsetLeft = slider.data('offset').left + masterLeft,
+            modifier = (masterOffsetLeft - pageX);
+        
+        slider.data('modifier', modifier);
       });
       
       // drag the handle to slide manually
       slider.bind('mousemove touchmove', function(e) {
+        e.preventDefault();
+        
         if (e.type == 'touchmove' || mousedown) {
+          slider.attr('data-dragging', 'true');
+          
           // normalize the pageX, pageY coordinates
           var pageX, pageY;
-          if (e.type == "touchstart") {
+          if (e.type == "touchmove") {
             pageX = e.originalEvent.targetTouches[0].pageX;
             pageY = e.originalEvent.targetTouches[0].pageY;
           } else {
@@ -179,11 +249,11 @@
               center      = slider.data('center');
               
           // calculate the new offset
-          var newOffset = pageX - (labelMaxWidth + 35),
+          var newOffset     = pageX + slider.data('modifier'),
               currentOffset = offset.left - (labelMaxWidth + 35);
               
           // move the slider within a fixed range
-          if ((newOffset > (currentOffset + 20)) && (newOffset <= (currentOffset - 20 + dimensions.width))) {
+          if ((newOffset > (currentOffset + 18)) && (newOffset <= (currentOffset - 16 + dimensions.width))) {
             master.offset({ left: newOffset });
           }
         }
@@ -195,16 +265,18 @@
       
       // slide to the "on" position
       slider.bind('slide:on', function() {
+        slider.data('animating', true).attr('data-dragging', 'false');
         master.stop().animate({ left: masterOn }, 'fast', function() {
-          slider.data('select').val(values.on);
+          slider.data('animating', false).data('select').val(values.on);
           slider.removeClass('off').addClass('on');
         });
       });
       
       // slide to the "off" position
       slider.bind('slide:off', function() {
+        slider.data('animating', true).attr('data-dragging', 'false');
         master.stop().animate({ left: masterOff }, 'fast', function() {
-          slider.data('select').val(values.off);
+          slider.data('animating', false).data('select').val(values.off);
           slider.removeClass('on').addClass('off');
         });
       });
