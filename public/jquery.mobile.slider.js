2ccc9b100206e4f70692c71236d73b87c13fdfc1
Mike Fulcher <mike@plan9design.co.uk>
2011-12-05 12:50:31 +0000

    Add disabled state and remove the dependency on background-size in preference for a media query to target retina displays

--- public/jquery.mobile.slider.js
+++ public/jquery.mobile.slider.js
@@ -121,11 +121,12 @@
   
   // main build function
   function buildSliderFromSelect(select, options) {
-    // hide the <select> and get it's initial val
-    var select  = select.hide(),
-        opts    = select.find('option'),
-        val     = select.val();
-
+    // hide the <select>, get it's initial val, and determine if it is disabled/readonly
+    var select    = select.hide(),
+        opts      = select.find('option'),
+        val       = select.val(),
+        disabled  = !!(select.attr('disabled') || select.attr('readonly'));
+        
     // get the on/off values and labels
     var values = {
       on: options.on || opts.first().val(),
@@ -134,18 +135,21 @@
       on: opts.filter('[value=' + values.on + ']').text(),
       off: opts.filter('[value=' + values.off + ']').text()
     };
-
+    
     // assign the <select>'s val as a class on the slider
     var slider = $(template.replace('{{on}}', text.on).replace('{{off}}', text.off));
     slider.addClass(select.val() == values.on ? 'on' : 'off');
-
+    
+    // if the <select> is disabled/readonly, add class 'disabled'
+    if (disabled) { slider.addClass('disabled'); }
+    
     // cache the master and handle elements
     var master = slider.find('.ui-slideToggle-master'),
         handle = slider.find('.ui-slideToggle-handle');
-
+        
     // insert the slider into the dom
     slider.insertAfter(select);
-
+    
     // find the max label width
     var labelMaxWidth = 0;
     slider.find('a').each(function(i, a) {
@@ -155,65 +159,63 @@
         labelMaxWidth = w;
       }
     });
-
+    
     // adjust the slider widths
     slider.find('.ui-slideToggle-middle').width(labelMaxWidth + 43);
     slider.find('a').width(labelMaxWidth);
-
+    
     // cache the "off" and "on" positions
     var masterOff = '-' + (labelMaxWidth + 19) + 'px',
         masterOn  = '0px';
-
+        
     // default to the "off" position
     master.css({ left: masterOff });
     handle.css({ left: (labelMaxWidth + 16) + 'px' });
-
+    
     // set the position to "on" based on the selected <option>
-    if (values.on == val) {
-      master.css({ left: masterOn });
-    }
-
+    if (values.on == val) { master.css({ left: masterOn }); }
+    
     // helper references between the original <select> and the slider widget
     select.data('slider', slider);
     slider.data('select', select);
-
+    
     // helpers to determine if the slider is currently in motion or being dragged
     slider.data('animating', false).attr('data-dragging', 'false');
-
+    
     // cache the offset, dimensions and center point of the slider widget
     slider
       .data('offset', slider.offset())
       .data('dimensions', { width: slider.width(), height: slider.height() })
       .data('center', { left: slider.data('offset').left + (slider.data('dimensions').width / 2), top: slider.data('offset').top + (slider.data('dimensions').height / 2) });
-
+      
     // cache the offset of the "master" and "handle" elements
     master.data('offset', master.offset());
     handle.data('offset', handle.offset());
-
+    
     // add controls to the slider widget
     slider.data('controls', {
-      on:  function() { slider.trigger('slide:on'); },
-      off: function() { slider.trigger('slide:off'); }
+      on:  function() { if (!disabled) { slider.trigger('slide:on');  } },
+      off: function() { if (!disabled) { slider.trigger('slide:off'); } }
     });
-
+    
     // watch for changes to the <select> and update the widget accordingly
     select.bind('change', function() {
       slider.data('controls')[select.val() == values.on ? 'on' : 'off']();
     });
-
+    
     // tap to toggle
     slider.bind('tap', function(e) {
       e.preventDefault();
-
+      
       if (!$(e.target).is('span')) {
         slider.data('controls')[slider.hasClass('on') ? 'off' : 'on']();
       }
     });
-
+    
     // "grab" the slider at a certain point when dragging starts
     slider.bind('mousedown touchstart', function(e) {
       e.preventDefault();
-
+      
       // normalize the pageX, pageY coordinates
       var pageX, pageY;
       if (e.type == "touchstart") {
@@ -223,7 +225,7 @@
         pageX = e.pageX;
         pageY = e.pageY;
       }
-
+      
       // calculate the offset based on which part of the slider was grabbed
       var masterLeft = parseInt(master.css('left')),
           masterOffsetLeft = slider.data('offset').left + masterLeft,
@@ -283,7 +285,7 @@
     // slide to the "on" position
     slider.bind('slide:on', function() {
       slider.data('animating', true).attr('data-dragging', 'false');
-      master.stop().animate({ left: masterOn }, 'fast', function() {
+      master.stop().animate({ left: masterOn, useTranslate3d:true }, 'fast', function() {
         slider.data('animating', false).data('select').val(values.on);
         slider.removeClass('off').addClass('on');
       });
@@ -292,7 +294,7 @@
     // slide to the "off" position
     slider.bind('slide:off', function() {
       slider.data('animating', true).attr('data-dragging', 'false');
-      master.stop().animate({ left: masterOff }, 'fast', function() {
+      master.stop().animate({ left: masterOff, useTranslate3d:true }, 'fast', function() {
         slider.data('animating', false).data('select').val(values.off);
         slider.removeClass('on').addClass('off');
       });
