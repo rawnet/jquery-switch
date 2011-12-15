a7a3575faa189594cc7ab8d03232d22dab60e5c1
Mike Fulcher <mike@plan9design.co.uk>
2011-12-15 13:12:52 +0000

    Update the test suite, and also test against both 1.4.0 and 1.7.1 of jquery for solid coverage.

--- jasmine/spec/jquery.switch.spec.js
+++ jasmine/spec/jquery.switch.spec.js
@@ -1,48 +1,121 @@
-// custom matchers
-beforeEach(function () {
-  this.addMatchers({
-    // test if an element is hidden
-    toBeHidden: function() {
-      return $(this.actual).is(':hidden');
-    },
-    // test if an element is visible
-    toBeVisible: function() {
-      return $(this.actual).is(':visible');
-    },
-    // test if a class is present
-    toHaveClass: function(classes) {
-      return $(this.actual).hasClass(classes);
-    }
-  });
-});
+var jQueryVersions = ['1.4.0', '1.7.1'];
 
-// spec suite
-describe('generic traits', function() {
-  // get the appropriate <select> this this suite
-  var $select;
+// run the test suite against multiple jquery versions
+for (var i = 0; i < jQueryVersions.length; i++) {
+  var version = jQueryVersions[i].replace(/\./g, '');
+  window.jQuery = window.$ = window['jQuery' + version];
   
-  beforeEach(function() {
-    $select = $('<select id="generic"><option value="1">On</option><option value="0">Off</option></select>').appendTo('body').switchify();
+  // custom matchers
+  beforeEach(function () {
+    this.addMatchers({
+      // test if an element is hidden
+      toBeHidden: function() {
+        return $(this.actual).is(':hidden');
+      },
+      // test if an element is visible
+      toBeVisible: function() {
+        return $(this.actual).is(':visible');
+      },
+      // test if a class is present
+      toHaveClass: function(classes) {
+        return $(this.actual).hasClass(classes);
+      }
+    });
   });
   
-  afterEach(function() {
-    $select.data('switch').remove();
-    $select.remove();
-    $select = undefined;
+  // spec suite: generic traits
+  // these specs test features which should be the same
+  // regardless of configuration
+  describe('generic traits (' + jQueryVersions[i] + ')', function() {
+    var $select, $switch;
+    
+    beforeEach(function() {
+      $select = $('<select><option value="1">On</option><option value="0">Off</option></select>').appendTo('body').switchify();
+      $switch = $select.data('switch');
+    });
+    
+    afterEach(function() {
+      $('select, .ui-switch').remove();
+      $select = undefined;
+      $switch = undefined;
+    });
+    
+    it('should be chainable', function() {
+      $select.switchify().addClass('chaining-test');
+      expect($select.hasClass('chaining-test')).toBeTruthy();
+    });
+    
+    it('should hide the original <select>', function() {
+      expect($select).toBeHidden();
+    });
+    
+    it('should insert the ui widget immediately after the <select>', function() {
+      var nextEl = $select.next();
+      expect(nextEl).toHaveClass('ui-switch');
+      expect(nextEl).toBeVisible();
+    });
+    
+    it('should create references between the select and the switch via .data()', function() {
+      var dataOnSelect = $select.data('switch'),
+          dataOnSwitch = $switch.data('select');
+      
+      expect(dataOnSelect[0]).toBe($switch[0]);
+      expect(dataOnSwitch[0]).toBe($select[0]);
+    });
+    
+    it('should normalize the width of the labels', function() {
+      var widths = $switch.find('a').map(function(i, a) { return $(a).width() });
+      expect(widths[0]).toEqual(widths[1]);
+    });
   });
   
-  it('should be chainable', function() {
-    $select.switchify().addClass('chaining-test');
-    expect($select.hasClass('chaining-test')).toBeTruthy();
+  // spec suite: <select> variations
+  // these test the default configuration against
+  // different variations of the original <select>
+  describe('select variations (' + jQueryVersions[i] + ')', function() {
+    beforeEach(function() {
+      
+    });
+    
+    afterEach(function() {
+      $('select, .ui-switch').remove();
+    });
+    
+    it('should use the <option> inner text as the labels', function() {
+      var $select = $('<select><option value="1">OnLabel</option><option value="0">OffLabel</option></select>').appendTo('body').switchify(),
+          $switch = $select.data('switch');
+          
+      var onLabel  = $switch.find('.ui-switch-on').text(),
+          offLabel = $switch.find('.ui-switch-off').text();
+      
+      expect(onLabel).toBe('OnLabel');
+      expect(offLabel).toBe('OffLabel');
+    });
+    
+    it('should assume that the first <option> represents the "on" state, and the last the "off" state', function() {
+      var $select = $('<select><option value="1">OnState</option><option value="0">OffState</option></select>').appendTo('body').switchify(),
+          $switch = $select.data('switch');
+          
+      expect($switch.find('.ui-switch-on').text()).toBe('OnState');
+      expect($switch.find('.ui-switch-off').text()).toBe('OffState');
+    });
+    
+    // loose matching only; testing the state of the switch is performed in another test suite
+    it('should assume the correct state depending on the selected <option>', function() {
+      var $select = $('<select><option value="1">On</option><option value="0">Off</option></select>').appendTo('body').switchify(),
+          $switch = $select.data('switch');
+          
+      expect($switch).toHaveClass('on');
+      
+      var $select = $('<select><option value="1" selected="selected">On</option><option value="0">Off</option></select>').appendTo('body').switchify(),
+          $switch = $select.data('switch');
+          
+      expect($switch).toHaveClass('on');
+      
+      var $select = $('<select><option value="1">On</option><option value="0" selected="selected">Off</option></select>').appendTo('body').switchify(),
+          $switch = $select.data('switch');
+          
+      expect($switch).toHaveClass('off');
+    });
   });
-  
-  it('should hide the original <select>', function() {
-    expect($select).toBeHidden();
-  });
-  
-  it('should insert the ui widget immediately after the <select>', function() {
-    var nextEl = $select.next();
-    expect(nextEl).toHaveClass('ui-switch');
-    expect(nextEl).toBeVisible();
-  });
-});
+}
